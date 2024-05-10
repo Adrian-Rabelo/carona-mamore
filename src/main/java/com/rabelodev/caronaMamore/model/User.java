d2672e519f350d68aa0942f32839f93833db7a29
Ádrian Rabelo <adrianrabeloe10@gmail.com>
2024-05-10 13:09:45 -0400

    criando primeira model para usuarios

---
+++ src/main/java/com/rabelodev/caronaMamore/model/User.java
@@ -0,0 +1,45 @@
+package com.rabelodev.caronaMamore.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name="users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name="uuid", unique = true)
+    private UUID uuid;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="login")
+    private String login;
+
+    @Column(name="password")
+    private String password;
+
+    @CreationTimestamp
+    @Column(name="created_at")
+    private LocalDateTime created_at;
+
+    @UpdateTimestamp
+    @Column(name="updated_at")
+    private LocalDateTime updated_at;
+}
