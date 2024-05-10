66a11970cb1a9b6ec98306fea1556c36382d0a4b
Ádrian Rabelo <adrianrabeloe10@gmail.com>
2024-05-10 12:19:11 -0400

    first commit

---
+++ src/main/java/com/rabelodev/caronaMamore/CaronaMamoreApplication.java
@@ -0,0 +1,13 @@
+package com.rabelodev.caronaMamore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaronaMamoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaronaMamoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rabelodev/caronaMamore/CaronaMamoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabelodev.caronaMamore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaronaMamoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
