002f63961c62bbe68bcb1178228aa120b17320df
Ádrian Rabelo <adrianrabeloe10@gmail.com>
2024-05-26 18:40:00 -0400

    refatora entidade do passageiro

---
+++ src/main/java/com/rabelodev/caronaMamore/entity/Passenger.java
@@ -0,0 +1,10 @@
+package com.rabelodev.caronaMamore.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "passengers")
+public class Passenger extends User {
+
+}
