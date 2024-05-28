0f7cbff519ccb626c502391e884e3c7b98d03ca4
Ádrian Rabelo <adrianrabeloe10@gmail.com>
2024-05-28 14:35:40 -0400

    renomeia chamada da interfacee

--- src/main/java/com/rabelodev/caronaMamore/repository/DriverRepository.java
+++ src/main/java/com/rabelodev/caronaMamore/repository/DriverRepository.java
@@ -1,14 +1,14 @@
 package com.rabelodev.caronaMamore.repository;
 
-import com.rabelodev.caronaMamore.dto.DriverDTO;
 import com.rabelodev.caronaMamore.entity.Driver;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.UUID;
 
 @Repository
-public interface DriverRepository extends JpaRepository<Driver, Long> {
+public interface DriverRepository extends JpaRepository<Driver, UUID> {
 
-//    List<DriverDTO> findAllDtoByOrderByIdAsc();
+    List<Driver> findAllDtoByOrderByIdAsc();
 }
