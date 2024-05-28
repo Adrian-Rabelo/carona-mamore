8b13d6e31d937d76bdf8ed70e067e3170dd459b6
Ádrian Rabelo <adrianrabeloe10@gmail.com>
2024-05-28 14:36:49 -0400

    inicia criacao do motorista

--- src/main/java/com/rabelodev/caronaMamore/service/DriverService.java
+++ src/main/java/com/rabelodev/caronaMamore/service/DriverService.java
@@ -1,7 +1,10 @@
 package com.rabelodev.caronaMamore.service;
 
-import com.rabelodev.caronaMamore.dto.DriverDTO;
+import com.rabelodev.caronaMamore.controller.dto.CreateDriverDto;
+import com.rabelodev.caronaMamore.controller.dto.CreateUserDto;
 import com.rabelodev.caronaMamore.entity.Driver;
+import com.rabelodev.caronaMamore.entity.User;
+import com.rabelodev.caronaMamore.exceptions.NullPasswordException;
 import com.rabelodev.caronaMamore.repository.DriverRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,70 +13,77 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class DriverService {
-//
-//    private static final Logger LOGGER = LoggerFactory.getLogger(DriverService.class);
-//
-//    @Autowired
-//    private DriverRepository repository;
-//
-//    public List<DriverDTO> getDrivers() {
-//        return repository.findAllDtoByOrderByIdAsc();
-//    }
-//
-//    /**
-//     * Create new driver and saves on database after validation
-//     *
-//     * @param driver
-//     * @return saved driver
-//     */
-//    public Driver createDriver(Driver driver) {
-//        validateDriver(driver);
-//
-//        return repository.save(driver);
-//    }
-//
-//    private void validateDriver(Driver driver) {
-//        LOGGER.info("Validating driver: " + driver.getName());
-//
-//        if (driver.getCnh() == null || driver.getCnh().isEmpty()) {
-//            // TODO: Create brazillian validation for cnh
-//            throw new IllegalStateException("Invalid national driver license");
-//        }
-//
-//        if (driver.getCarLicensePlate() == null || driver.getCarLicensePlate().isEmpty()) {
-//            throw new IllegalStateException("Invalid driver licence plate");
-//        }
-//
-//        // TODO: Finish validate driver method;
-//    }
-//
-//    /**
-//     * Update driver datas
-//     *
-//     * @param id
-//     * @param driver
-//     * @return
-//     */
-//
-//    public Optional<Driver> updateDriver(Long id, Driver driver) {
-//        // TODO: create validation method for fields (?)
-//        return repository.findById(id)
-//            .map(existingUser -> {
-//                existingUser.setName(driver.getName());
-//                existingUser.setEmail(driver.getEmail());
-//                existingUser.setUsername(driver.getUsername());
-//                existingUser.setCnh(driver.getCnh());
-//                existingUser.setCarColor(driver.getCarColor());
-//                existingUser.setCarModel(driver.getCarModel());
-//                existingUser.setCarLicensePlate(driver.getCarLicensePlate());
-//                existingUser.setStreet(driver.getStreet());
-//                existingUser.setNeighborhood(driver.getNeighborhood());
-//                existingUser.setHouseNumber(driver.getHouseNumber());
-//
-//                return repository.save(existingUser);
-//            });
-//    }
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DriverService.class);
+
+    @Autowired
+    private DriverRepository driverRepository;
+    @Autowired
+    private UserService userService;
+
+    public List<Driver> getDrivers() {
+        return driverRepository.findAllDtoByOrderByIdAsc();
+    }
+
+    /**
+     * Create new driver and saves on database after validation
+     *
+     * @param driver
+     * @return saved driver
+     */
+    public UUID createDriver(Driver driver) {
+
+//        var driver = new Driver();
+
+        var savedDriver = driverRepository.save(driver);
+
+        return savedDriver.getId();
+
+    }
+
+    private void validateDriver(Driver driver) {
+        LOGGER.info("Validating driver: " + driver.getName());
+
+        if (driver.getCnh() == null || driver.getCnh().isEmpty()) {
+            // TODO: Create brazillian validation for cnh
+            throw new IllegalStateException("Invalid national driver license");
+        }
+
+        if (driver.getCarLicensePlate() == null || driver.getCarLicensePlate().isEmpty()) {
+            throw new IllegalStateException("Invalid driver licence plate");
+        }
+
+        // TODO: Finish validate driver method;
+    }
+
+    /**
+     * Update driver datas
+     *
+     * @param id
+     * @param driver
+     * @return
+     */
+
+    public Optional<Driver> updateDriver(UUID uuid, Driver driver) {
+        // TODO: create validation method for fields (?)
+        return driverRepository.findById(uuid)
+            .map(existingUser -> {
+                existingUser.setName(driver.getName());
+                existingUser.setEmail(driver.getEmail());
+                existingUser.setUsername(driver.getUsername());
+                existingUser.setCnh(driver.getCnh());
+                existingUser.setCarColor(driver.getCarColor());
+                existingUser.setCarModel(driver.getCarModel());
+                existingUser.setCarLicensePlate(driver.getCarLicensePlate());
+                existingUser.setStreet(driver.getStreet());
+                existingUser.setNeighborhood(driver.getNeighborhood());
+                existingUser.setHouseNumber(driver.getHouseNumber());
+
+                return driverRepository.save(existingUser);
+            });
+    }
 }
