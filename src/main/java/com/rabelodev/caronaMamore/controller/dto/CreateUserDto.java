7c9b3b8efd7cb0c570c0dce7c60c77fb726f4c74
Ádrian Rabelo <adrianrabeloe10@gmail.com>
2024-05-28 14:34:06 -0400

    cria data transfer object do usuario

--- src/main/java/com/rabelodev/caronaMamore/controller/dto/CreateUserDto.java
+++ src/main/java/com/rabelodev/caronaMamore/controller/dto/CreateUserDto.java
@@ -1,14 +1,15 @@
 package com.rabelodev.caronaMamore.controller.dto;
 
+import com.rabelodev.caronaMamore.entity.enums.Gender;
 import lombok.Builder;
 
 import java.time.LocalDateTime;
-import java.util.UUID;
 
 @Builder
-public record CreateUserDTO(String name,
+public record CreateUserDto(String name,
                             String cpf,
                             String rg,
+                            Gender gender,
                             LocalDateTime bornDate,
                             String email,
                             String username,
