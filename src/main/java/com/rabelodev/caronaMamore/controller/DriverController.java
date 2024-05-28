e74446d25d9ebd710eb665a071ae46f964fef722
Ádrian Rabelo <adrianrabeloe10@gmail.com>
2024-05-28 14:36:12 -0400

    prepara criacao do motorista

--- src/main/java/com/rabelodev/caronaMamore/controller/DriverController.java
+++ src/main/java/com/rabelodev/caronaMamore/controller/DriverController.java
@@ -1,7 +1,8 @@
 package com.rabelodev.caronaMamore.controller;
 
-import com.rabelodev.caronaMamore.dto.DriverDTO;
+import com.rabelodev.caronaMamore.controller.dto.CreateDriverDto;
 import com.rabelodev.caronaMamore.entity.Driver;
+import com.rabelodev.caronaMamore.entity.User;
 import com.rabelodev.caronaMamore.repository.DriverRepository;
 import com.rabelodev.caronaMamore.service.DriverService;
 import org.slf4j.Logger;
@@ -10,40 +11,48 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/driver")
 public class DriverController {
 
-//    private static final Logger LOGGER = LoggerFactory.getLogger(DriverController.class);
-//
-//    @Autowired
-//    private DriverRepository repository;
-//
-//    @Autowired
-//    private DriverService service;
-//
-//    @PostMapping("/create")
-//    public Driver createDriver(@RequestBody Driver driver) throws Exception {
-//        LOGGER.info("Creating driver");
-//
-//        return service.createDriver(driver);
-//    }
-//
-//    @GetMapping()
-//    public List<DriverDTO> getDrivers() {
-//        LOGGER.info("Listing all drivers");
-//        return service.getDrivers();
-//
-//    }
-//
-//    @PutMapping("/{id}")
-//    public ResponseEntity<Driver> updateDriver(@PathVariable Long id, @RequestBody Driver updateDriver) {
-//        Optional<Driver> updatedDriver = service.updateDriver(id, updateDriver);
-//
-//        return updatedDriver.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
-//    }
+    private static final Logger LOGGER = LoggerFactory.getLogger(DriverController.class);
+
+    @Autowired
+    private DriverRepository driverRepository;
+
+    @Autowired
+    private DriverService driverService;
+
+
+    // TODO: Refinar lógica da criação de motorista
+    @PostMapping("/create")
+    public ResponseEntity<Driver> createDriver(@RequestBody Driver driver) throws Exception {
+        LOGGER.info("Creating driver");
+
+        var driverId = driverService.createDriver(driver);
+
+        return ResponseEntity.created(URI.create("/driver/create" + driverId.toString())).build();
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<Driver>> getDrivers() {
+        LOGGER.info("Listing all drivers");
+        var listAll = driverService.getDrivers();
+
+        return ResponseEntity.ok(listAll);
+
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Driver> updateDriver(@PathVariable UUID id, @RequestBody Driver updateDriver) {
+        Optional<Driver> updatedDriver = driverService.updateDriver(id, updateDriver);
+
+        return updatedDriver.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
 
 }
