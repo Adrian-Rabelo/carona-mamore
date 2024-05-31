512ede8a97c598683f3ec4dacc40a7aebcb9d0c6
Ádrian Rabelo <adrianrabeloe10@gmail.com>
2024-05-31 16:28:53 -0400

    refatora entidade driver

--- src/main/java/com/rabelodev/caronaMamore/entity/Driver.java
+++ src/main/java/com/rabelodev/caronaMamore/entity/Driver.java
@@ -1,38 +1,77 @@
 package com.rabelodev.caronaMamore.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.UUID;
 
 @Entity
-@Getter
-@Setter
+@Builder
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "drivers")
-public class Driver extends User {
+public class Driver {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", unique = true)
+    private UUID id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "cpf", nullable = false, unique = true)
+    private String cpf;
+
+    @Column(name = "rg", nullable = false, unique = true)
+    private String rg;
 
     @Column(name = "national_driving_license")
     private String cnh;
 
-    @Column(name = "car_year")
-    private String carYear;
+    @Column(name = "birth_date")
+    private Date birthDate;
 
-    @Column(name = "car_brand")
-    private String carBrand;
+    @Column(name = "gender")
+    private String gender;
 
-    @Column(name = "car_model")
-    private String carModel;
+    @Column(name = "email")
+    private String email;
 
-    @Column(name = "car_color")
-    private String carColor;
+    // TODO : Create auto blob generation for username (user-name, jhon-doe)
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
 
     @Column(name = "car_license_plate")
     private String carLicensePlate;
-    
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @ManyToOne()
+    private Car car;
+
+    @Column(name = "photo")
+    private byte[] photo;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    @Temporal(value = TemporalType.TIMESTAMP)
+    private Date createdAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    @Temporal(value = TemporalType.TIMESTAMP)
+    private Date updatedAt;
+
+
 }
